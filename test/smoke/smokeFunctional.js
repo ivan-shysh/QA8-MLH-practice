a9fd1190a3cd83530a4e459e3b22feb183ce743c
ivansh83 <ivansh83@gmail.com>
2021-03-27 15:50:38 -0400

    added testData, removed hard code from TC-026

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,4 +1,5 @@
 import sel from '../../data/selectors';
+import {name, gender, age, story} from '../../data/testData'
 
 
 describe('Required fields and story created', function () {
@@ -9,11 +10,11 @@ describe('Required fields and story created', function () {
 
 
     it('TC-026 Submit button is enabled after fields 1-4 are filled with valid input', function () {
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('123456789');
+        $(sel.name).setValue(name.default);
+        $$(sel.radioButtons)[gender.she].click();
+        $(sel.age).setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        $$(sel.storyList)[story.comedy].click();
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     });
--- wdio.conf.js
+++ wdio.conf.js
@@ -22,8 +22,6 @@ exports.config = {
     ],
     // Patterns to exclude.
     exclude: [
-        './test/smoke/elementsExist.js',
-        './test/smoke/labelsCorrect.js'
 
     ],
     //
@@ -93,7 +91,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://qa-apps.netlify.app/hero',
+    baseUrl: 'https://qa-apps.netlify.app/hero/fix',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
